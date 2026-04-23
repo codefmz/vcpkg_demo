434d041e2f1935b335c26f98bdcd5a847c339587
fengmowei <fengmowei@creality.com>
2026-04-23 15:20:07 +0800

    vcpkg lib demo init

---
+++ vcpkgsample.cpp
@@ -0,0 +1,7 @@
+#include "vcpkgsample.h"
+#include <fmt/core.h>
+
+void vcpkgSample(const char *name)
+{
+    fmt::print("Hello {}\n", name);
+}
---
+++ vcpkgsample.h
@@ -0,0 +1,7 @@
+#ifndef VCPKGSAMPLE_H
+#define VCPKGSAMPLE_H
+
+
+void vcpkgSample(const char *name);
+
+#endif // VCPKGSAMPLE_H
